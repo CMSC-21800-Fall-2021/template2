45db72697116b656566bc5a99330ffe55b6d0782
Sanjay Krishnan <sanjaykrishn@gmail.com>
2021-11-09 09:25:35 -0600

    Create scratch.py

---
+++ explore/scratch.py
@@ -0,0 +1,14 @@
+'''You will write down any "scratch" code that you use to explore the dataset in
+this file. This code does not produce any outputs (it does not modify any files)
+but allows you to explore the data. We're separating this code from the rest because 
+it allows us to understand your thought process during data exploration.
+'''
+
+
+def clean(df):
+  '''clean(df) takes as input a dataframe and fixes any data errors  
+     *that might affect your results*. it returns a copy of the dataframe 
+     without errors.
+  '''
+  
+  return df
